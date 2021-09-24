2d121cf7f908214842344834fd3073eaaf537e0a
Alex Krzos <akrzos@redhat.com>
2021-09-24 10:55:45 -0400

    Add test cases

---
+++ tests/nodedensity/testcase-6.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Node Density Enhanced Testing
+# Test Case 6 - Route Test
+
+nodes=$(oc get no -l jetlag=true --no-headers | wc -l)
+node_pods=5
+total_pods=$((${nodes} * ${node_pods}))
+
+mkdir -p ../logs
+sleep_period=120
+
+gohttp_env_vars="-e LISTEN_DELAY_SECONDS=0 LIVENESS_DELAY_SECONDS=0 READINESS_DELAY_SECONDS=0 RESPONSE_DELAY_MILLISECONDS=0 LIVENESS_SUCCESS_MAX=0 READINESS_SUCCESS_MAX=0"
+measurement="-D 180"
+csvfile="--csv-file tc6-$1-$(date -u +%Y%m%d-%H%M%S).csv"
+
+# Debug/Test entire Run
+# dryrun="--dry-run"
+measurement="--no-measurement-phase --no-cleanup-phase"
+# sleep_period=1
+
+echo "$(date -u +%Y%m%d-%H%M%S) - Test Case 6 Start"
+echo "$(date -u +%Y%m%d-%H%M%S) - Total Pod Count (Nodes * 500pods/node) :: ${nodes} * ${node_pods} = ${total_pods}"
+echo "****************************************************************************************************************************************"
+
+probes="--startup-probe http,0,10,1,12 --liveness-probe http,0,10,1,3 --readiness-probe http,0,10,1,3,1"
+
+echo "$(date -u +%Y%m%d-%H%M%S) - node density 6.1 - ${total_pods} namespaces, 1 deploy, 1 pod, 1 container (gohttp), 1 service, 1 route, 10s period probes, 0 configmaps, 0 secrets"
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-nodedensity-6.1.log"
+../../jetkubeload/jetkubeload.py ${dryrun} ${csvfile} --csv-title "${total_pods}n-1d-1p-1c-gohttp" -n ${total_pods} -d 1 -p 1 -c 1 -m 0 --secrets 0 ${probes} ${gohttp_env_vars} ${measurement} ${INDEX_ARGS} &> ${logfile}
+echo "$(date -u +%Y%m%d-%H%M%S) - node density 6.1 complete, sleeping ${sleep_period}"
+# sleep ${sleep_period}
+
+echo "$(date -u +%Y%m%d-%H%M%S) - Test Case 6 Complete"
---
+++ tests/rwn/testcase-4.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+# Test Case 4 - Latency and Packet Loss
+
+mkdir -p ../logs
+sleep_period=120
+pods=1400
+
+# RWN Workload - Guaranteed Small Pods, 1000 labels 50ms latency w/ and w/o tolerations
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.1.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 --no-tolerations -D 1800 -L 50 -P 0 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.2.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 -D 1800 -L 50 -P 0 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+# RWN Workload - Guaranteed Small Pods, 1000 labels 200ms latency w/ and w/o tolerations
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.3.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 --no-tolerations -D 1800 -L 200 -P 0 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.4.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 -D 1800 -L 200 -P 0 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+# RWN Workload - Guaranteed Small Pods, 1000 labels 1000ms latency w/ and w/o tolerations
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.5.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 --no-tolerations -D 1800 -L 1000 -P 0 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.6.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 -D 1800 -L 1000 -P 0 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+# RWN Workload - Guaranteed Small Pods, 1000 labels 3000ms latency w/ and w/o tolerations
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.7.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 --no-tolerations -D 1800 -L 3000 -P 0 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.8.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 -D 1800 -L 3000 -P 0 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+# RWN Workload - Guaranteed Small Pods, 1000 labels 2% Packet Loss w/ and w/o tolerations
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.9.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 --no-tolerations -D 1800 -L 0 -P 2 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.10.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 -D 1800 -L 0 -P 2 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+# RWN Workload - Guaranteed Small Pods, 1000 labels 5% Packet Loss w/ and w/o tolerations
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.11.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 --no-tolerations -D 1800 -L 0 -P 5 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.12.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 -D 1800 -L 0 -P 5 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+# RWN Workload - Guaranteed Small Pods, 1000 labels 10% Packet Loss w/ and w/o tolerations
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.13.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 --no-tolerations -D 1800 -L 0 -P 10 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.14.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 -D 1800 -L 0 -P 10 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+# RWN Workload - Guaranteed Small Pods, 1000 labels 20% Packet Loss w/ and w/o tolerations
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.15.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 --no-tolerations -D 1800 -L 0 -P 20 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+sleep ${sleep_period}
+
+logfile="../logs/$(date -u +%Y%m%d-%H%M%S)-rwn-4.16.log"
+../../jetkubeload/jetkubeload.py -n ${pods} --cpu-limits 100 --memory-limits 1024 -s 1000 -u 1000 -D 1800 -L 0 -P 20 -F 0 -U 0 -T ${RWN_INDEX_ARGS} 2>&1 | tee ${logfile}
+echo "****************************************************************************************************************************************"
+
+echo "Test Case 4 Complete"
